36c7a8ae736265e7c2ef29e4adf9ec2868651245
shreeteja22 <smutukundu2206@gmail.com>
2025-07-31 18:19:15 +0530

    fix: fixed core issues

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,7 @@ config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL, process.env.BACKEND_URL],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api/", mainRouter);
 app.use("/api", uploadthingRoutes);
