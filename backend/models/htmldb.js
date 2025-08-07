19600b0bf7f0e32557affc3bfe59601c1402569b
shreeteja22 <smutukundu2206@gmail.com>
2025-08-07 09:03:54 +0530

    fix: html schema not needed to store in db

--- backend/models/htmldb.js
+++
@@ -1,9 +0,0 @@
-// import mongoose from "mongoose";
-
-// const htmlPostSchema = new mongoose.Schema({
-//   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-//   htmlContent: { type: String, required: true },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// export const HtmlPost = mongoose.model("HtmlPost", htmlPostSchema);
